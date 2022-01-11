c22d2c19360ed7c932b2724d32040c804a8e9953
spdz <spidepog@gmail.com>
2022-01-11 21:14:49 +0300

    Tweaked Notebook

--- presets.py
+++ presets.py
@@ -1,5 +1,6 @@
 from bio import Preset
 
+Default = Preset()
 Mamal = Preset(leg_count=2, straightness=(6, 20), gradation=(10, 50))
 Insect = Preset(leg_count=3, straightness=(0.01, 10), distribution=(0.01, 20), gradation=(0.01, 5))
 Bird = Preset(leg_count=1, straightness=(1, 15), gradation=(1, 50), length=(6, 9))
